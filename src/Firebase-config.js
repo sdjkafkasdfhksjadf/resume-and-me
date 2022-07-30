aab5bc6e00460e8460e3ca00e8a6cc8f771b77e2
philipyeh12 <philipyeh12@yahoo.com>
2022-07-30 15:50:22 -0400

    Login with firebase set up

--- src/Firebase-config.js
+++ src/Firebase-config.js
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,23 @@ import { Form , Button, Card} from 'react-bootstrap'
 
 function App() {
   return (
-
+  <>
     <Card>
-      
-    </Card>
+      <Card.Body>
+        <h2 className="text-center mb-4"> sign up </h2>
+        <Form>
+          <Form.Group id="email">Email</Form.Group>
+        </Form>
+      </Card.Body>
 
+    </Card>
+    <div className=" w-100 text-center mt-2">
+      Already have a account? Log in
+
+
+    </div>
+
+  </>
 
   )
     
