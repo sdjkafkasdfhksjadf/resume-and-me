8598fd97bbb6741fb026b7f4c7981f641faadd94
philipyeh12 <philipyeh12@yahoo.com>
2022-08-01 13:45:05 -0400

    Login/Signup Authenticating and Routing *working on nav*

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,21 @@ import Signup from "./Signup";
 import Container from "react-bootstrap/esm/Container";
 import {AuthProvider} from "../Context/Auth-Context"
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Link, useNavigate} from "react-router-dom"
 import Homepage from "./Homepage"
 import Login from "./Login"
+import { useState } from "react";
+
 
 
 function App() {
+
+  
+
+
+
   return (
+
     
       <Container 
         className = "d-flex align-items-center justify-content-center"
@@ -17,6 +26,12 @@ function App() {
       >
         <div className="w-100" style={{maxWidth: "400px"}}>
           <Router>
+            <nav>
+            <Link to="/login"> Logout </Link>
+            <Link to="/ResumeBuilder"> ResumeBuilder </Link>
+
+
+            </nav>
             <AuthProvider>
               <Routes>
                 <Route path ="/" element = {<Signup/>}/>
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Form , Button, Card, Alert} from 'react-bootstrap'
 
 export default function Homepage() {
   return (
