50ce002f205d47aff68f318b17eb77a645963c86
philipyeh12 <philipyeh12@yahoo.com>
2022-08-01 20:17:12 -0400

    Login/Signup/Logout authentification functions with navbar and protected routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-
+import { Nav } from "react-bootstrap"
+import NavBar from 'react-bootstrap/Navbar'
 import React from "react";
 import Signup from "./Signup";
 import Container from "react-bootstrap/esm/Container";
@@ -7,15 +8,17 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {Link, useNavigate} from "react-router-dom"
 import Homepage from "./Homepage"
 import Login from "./Login"
+import Contact from "./Contact"
+import OldResume from "./OldResume"
 import { useState } from "react";
 import PrivateRoute from "./PrivateRoute";
+import ResumeBuilder from "./ResumeBuilder";
 
 
 
 function App() {
 
   
-
 
 
   return (
@@ -25,17 +28,35 @@ function App() {
         className = "d-flex align-items-center justify-content-center"
         style={{minHeight: "100vh"}}
       >
+      <NavBar fixed = "top" bg = "dark" variant ="dark">
+        <Container>
+          <NavBar.Brand hfref= "Navibar">Resume and Me</NavBar.Brand>
+            <Nav>
+              <Nav.Link href = "/">Home</Nav.Link>
+              <Nav.Link href = "/OldResume">Old Resume</Nav.Link>
+              <Nav.Link href = "/Contact">Contact</Nav.Link>
+              <Nav.Link href= "">Log Out</Nav.Link>
+            </Nav>
+        </Container>
+      </NavBar>
+
         <div className="w-100" style={{maxWidth: "400px"}}>
           <Router>
-
           
             <AuthProvider>
+            
               <Routes>
                 <Route element={<PrivateRoute />}>
                   <Route path="/" element={<Homepage />} />
+                  <Route path ="/resumebuilder" element = {<ResumeBuilder/>}/>
+                  <Route path = "/Contact" element = {<Contact/>}/>
+                  <Route path = "/OldResume" element = {<OldResume/>}/>
+                  
                 </Route>
                 <Route path ="/signup" element= {<Signup/>}/>
                 <Route path ="/login" element = {<Login/>}/>
+                
+                
               </Routes>
             </AuthProvider>
           </Router>
