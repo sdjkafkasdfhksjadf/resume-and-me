335077b38cdfc12c70eef1f883d6d14fcda2be20
sdjkafkasdfhksjadf <106289739+sdjkafkasdfhksjadf@users.noreply.github.com>
2022-08-08 18:18:27 -0400

    Update GetData.js

--- src/components/GetData.js
+++ src/components/GetData.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
-export default function GetData() {
+function CreatePost() {
+
+  const [company, setCompany] = useState("");
+  const [jobTitle, setJobTitle] = useState("");
+
+  const postsCollectionRef = collection(db, "DataSearchInfo");
+  let navigate = useNavigate();
+
+  const createPost = async () => {
+    await addDoc(postsCollectionRef, {
+      company,
+      jobTitle
+    });
+    
+  };
+
+  
+  
   return (
-    <div>GetData</div>
-  )
+    <div className="dataSearchPage">
+      <div className="dataSearchContainer">
+        <h1>Search for Job Statistics</h1>
+
+        <div className="question">
+          <label> Company:</label>
+          <input
+             placeholder="Company..."
+             onChange={(event) => {
+               setCompany(event.target.value);
+             }}
+          />
+        </div>
+
+        <div className="question">
+          <label>Job Title:</label>
+          <input
+             placeholder="Job title..."
+             onChange={(event) => {
+               setJobTitle(event.target.value);
+             }}
+          />
+        </div>
+        </div>
+        <button onClick={createPost}> Submit Post</button>
+        
+      </div>
+    
+  );
 }
+
+export default CreatePost;
